245e3fd6a319f261435d747d49e57d3e44aad329
Viki Val <viktorko99@gmail.com>
2024-09-13 21:51:22 +0200

    Refactor site.config.ts to update site metadata

---
+++ config/site.config.ts
@@ -0,0 +1,13 @@
+const siteMeta = {
+  title: "genstagram",
+  description: "Ranked feed of gen-art channel",
+  domain: "www.dietcast.xyz",
+  websiteUrl: "https://genstagram.koda.art",
+  channelUrl: "https://warpcast.com/~/channel/koda",
+  channelName: "koda",
+  logo: "/logo.svg",
+  ogImage: "/og.png",
+  favicon: "/favicon.png",
+};
+
+export default siteMeta;
